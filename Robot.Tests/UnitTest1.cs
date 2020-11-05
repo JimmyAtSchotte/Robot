8637eede93103d0409ec87d31aaa55d29d7d31b5
Jimmy <jimmy.mattsson@schotte.se>
2020-11-05 19:13:17 +0100

    Move robot

--- Robot.Tests/UnitTest1.cs
+++ Robot.Tests/UnitTest1.cs
@@ -1,10 +1,14 @@
 using NUnit.Framework;
+using System;
 
 namespace Robot.Tests
 {
     public class Robot
     {
-
+        internal void Move(string v)
+        {
+            
+        }
     }
 
 
@@ -14,6 +18,13 @@ namespace Robot.Tests
         public void CreateRobot()
         {
             var robot = new Robot();
+        }
+
+        [Test]
+        public void MoveRobot()
+        {
+            var robot = new Robot();
+            robot.Move("N 1");
         }
     }
 }
